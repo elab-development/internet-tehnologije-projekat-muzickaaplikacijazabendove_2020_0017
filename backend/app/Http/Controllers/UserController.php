d5f49860b2a6bf7bb53d2d11b214166c8e216c2b
anjaajovanovic <jovanovicanjaa01@gmail.com>
2024-07-08 21:02:49 +0200

    Ispravka gresaka

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       $users = User::all();
+       return $users;
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id) 
+    {
+        $user = User::find($id);
+
+        if(is_null($user)) {
+            return response() -> json(['error' => 'User not found'], 404);
+        }
+
+        return response() -> json($user);
+    }
+
+
+}
